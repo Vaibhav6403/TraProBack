bb4f52081b9ab1bc3fb0bf9dcbf90d74a01863fd
Vaibhav6403 <vaibhavchauhan807@gmail.com>
2025-07-07 14:28:10 +0530

    adding in index.js

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ connectdb();
 
 app.use(express.static(path.join(__dirname, '../frontend-dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend-dist', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/user',router)
