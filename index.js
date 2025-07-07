738dbe99a5d0bd83af389ab6a3ef50666dc45a3a
Vaibhav6403 <vaibhavchauhan807@gmail.com>
2025-07-07 14:46:18 +0530

    commented the * route

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ connectdb();
 app.use('/api/user',router)
 app.use(express.static(path.join(__dirname, '../frontend-dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend-dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend-dist', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 5000;
 
